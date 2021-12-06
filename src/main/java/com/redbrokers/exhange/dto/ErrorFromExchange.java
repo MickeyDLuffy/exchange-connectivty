b7f47e8c69026c12d060d9bb773310aad40ccef0
Mickey D. Luffy <thefalconslayer@gmail.com>
2021-12-06 00:22:33 +0000

    Some few methods

---
+++ src/main/java/com/redbrokers/exhange/dto/ErrorFromExchange.java
@@ -0,0 +1,20 @@
+package com.redbrokers.exhange.dto;
+
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonPOJOBuilder;
+import lombok.Builder;
+import lombok.Value;
+
+@Value
+@Builder(toBuilder = true)
+@JsonDeserialize(builder = ErrorFromExchange.ErrorFromExchangeBuilder.class)
+public class ErrorFromExchange {
+//    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern ="yyyy-MM-dd'T'HH:mm:ss" )
+    String timestamp;
+    String status;
+    String error;
+    String message;
+    String path;
+    @JsonPOJOBuilder(withPrefix = "")
+    public static class ErrorFromExchangeBuilder {}
+}
--- src/main/java/com/redbrokers/exhange/service/impl/ExchangeConnectivityServiceImpl.java
+++ src/main/java/com/redbrokers/exhange/service/impl/ExchangeConnectivityServiceImpl.java
@@ -1,15 +1,15 @@
 package com.redbrokers.exhange.service.impl;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.redbrokers.exhange.dto.ErrorFromExchange;
 import com.redbrokers.exhange.dto.ErrorMessage;
 import com.redbrokers.exhange.dto.FullOrderBook;
 import com.redbrokers.exhange.dto.Order;
 import com.redbrokers.exhange.service.ExchangeConnectivityService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.server.ResponseStatusException;
@@ -25,10 +25,13 @@ import java.util.UUID;
 @Service
 @RequiredArgsConstructor
 public class ExchangeConnectivityServiceImpl implements ExchangeConnectivityService {
+    @Value("${exchange.variables.exchange-url.order-url}")
+    private String orderBaseUrl;
     @Value("${exchange.variables.exchange-url.one}")
     private String exchangeOneUrl;
     @Value("${exchange.variables.exchange-url.two}")
     private String exchangeTwoUrl;
+
     private final ObjectMapper objectMapper = new ObjectMapper();
     private final RestTemplate restTemplate;
     ErrorMessage.ErrorMessageBuilder errorMessage = ErrorMessage.builder();
@@ -37,10 +40,11 @@ public class ExchangeConnectivityServiceImpl implements ExchangeConnectivityServ
         /**
          * Used java 9's HttpClient experimentally, for the api call. We can change this later
          */
-        var orderToJson = objectMapper.convertValue(Order.class, String.class);
+        String orderToJson = null;
         try {
+            orderToJson = objectMapper.writeValueAsString(order);
             HttpRequest request = HttpRequest.newBuilder()
-                    .uri(new URI(exchangeOneUrl))
+                    .uri(new URI(exchangeOneUrl + orderBaseUrl))
                     .header("Content-Type", MediaType.APPLICATION_JSON_VALUE)
                     .POST(HttpRequest.BodyPublishers.ofString(orderToJson))
                     .build();
@@ -49,8 +53,26 @@ public class ExchangeConnectivityServiceImpl implements ExchangeConnectivityServ
                     .build()
                     .send(request, HttpResponse.BodyHandlers.ofString());
 
+
+            if(response.statusCode() == HttpStatus.NOT_FOUND.value()) {
+                return new ResponseEntity<>(ErrorMessage.builder()
+                        .message("The exchange may not have this route")
+                        .httpStatus(HttpStatus.NOT_FOUND).build(), HttpStatus.NOT_FOUND);
+            }
+            if(response.statusCode() == HttpStatus.BAD_REQUEST.value()) {
+                return new ResponseEntity<>(objectMapper.readValue(response.body(), ErrorFromExchange.class),
+                        HttpStatus.INTERNAL_SERVER_ERROR);
+            }
+
+            if(response.statusCode() == HttpStatus.INTERNAL_SERVER_ERROR.value()) {
+                var ms = objectMapper.readValue(response.body(), ErrorFromExchange.class);
+                return new ResponseEntity<>(ms, HttpStatus.INTERNAL_SERVER_ERROR);
+            }
             return new ResponseEntity<>(response.body(), HttpStatus.CREATED);
-        } catch (URISyntaxException ex) {
+        } catch (JsonProcessingException ex) {
+            System.out.println(ex.getMessage());
+            return new ResponseEntity<>(ex, HttpStatus.REQUEST_TIMEOUT);
+        }  catch (URISyntaxException ex) {
            var error = ErrorMessage.builder()
                    .httpStatus(HttpStatus.BAD_REQUEST)
                    .message("Poorly formed Exchange URL. Confirm that the url is correct")
@@ -74,27 +96,44 @@ public class ExchangeConnectivityServiceImpl implements ExchangeConnectivityServ
 
     @Override
     public ResponseEntity<?> checkOrderStatus(UUID orderId) {
-        var response = restTemplate.getForEntity(exchangeOneUrl + "/" + orderId, FullOrderBook.class);
+        try {
+            var url = exchangeOneUrl + orderBaseUrl+ orderId;
+            var response = restTemplate.exchange(url, HttpMethod.GET, null, FullOrderBook.class);
 
-        if(response.getStatusCode().is2xxSuccessful()) {
-            return new ResponseEntity<>(response.getBody(), HttpStatus.OK);
+            if(response.getStatusCode().is2xxSuccessful()) {
+                return new ResponseEntity<>(response.getBody(), HttpStatus.OK);
+            }
+
+            if(response.getStatusCodeValue() == HttpStatus.NOT_FOUND.value()) {
+                var error = errorMessage.message("The Order with id" + orderId + " does not exist")
+                        .httpStatus(HttpStatus.NOT_FOUND)
+                        .build();
+                return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+            }
+            if(response.getStatusCode().is5xxServerError()) {
+                return  new ResponseEntity<>(objectMapper.convertValue(response.getBody(), ErrorFromExchange.class),
+                        HttpStatus.INTERNAL_SERVER_ERROR);
+            }
+            return null;
+        } catch (Exception e) {
+          return new ResponseEntity<>(e, HttpStatus.INTERNAL_SERVER_ERROR);
         }
-        if(response.getStatusCodeValue() == HttpStatus.NOT_FOUND.value()) {
-            var error = errorMessage.message("The Order with id" + orderId + " does not exist")
-                    .httpStatus(HttpStatus.NOT_FOUND)
-                    .build();
-            return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
-        }
-        throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "The exchange could not be reached");
+
+//        return new ResponseEntity<>();
     }
 
     @Override
     public ResponseEntity<?> cancelOrder(UUID orderId) {
-        return null;
+        var url = exchangeOneUrl + orderBaseUrl+ orderId;
+
+        return restTemplate.exchange(url, HttpMethod.DELETE, null, Boolean.class);
     }
 
     @Override
     public ResponseEntity<?> changeOrder(Order order, UUID orderId) {
-        return null;
+        var url = exchangeOneUrl + orderBaseUrl+ orderId;
+        HttpHeaders httpHeaders = new HttpHeaders();
+        HttpEntity<?> httpEntity = new HttpEntity<>(order, httpHeaders);
+        return restTemplate.exchange(url, HttpMethod.PUT, httpEntity, Boolean.class);
     }
 }
